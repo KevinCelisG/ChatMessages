bc507faadd1e72bcc7d1baa2e0d64616fcbd0e15
kevincelis <celis.kevin@gmail.com>
2025-05-11 08:07:43 -0500

    Project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.readyChatAI.readyChatMessages"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.readyChatAI.readyChatMessages"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/readyChatAI/readyChatMessages/ReadyChatMessagesApplication.kt
@@ -0,0 +1,7 @@
+package com.readyChatAI.readyChatMessages
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReadyChatMessagesApplication : Application()
